d8bf027776eb749c5e8f353f7f9d3d174b23792c
matetipping <matetipping@gmail.com>
2018-11-29 15:09:05 +1100

    Update script.js

--- script.js
+++ script.js
@@ -35,9 +35,17 @@ function attemptLogIn(username, tokenCount) {
 		$(".username-container span span:nth-child(1)").html("<b>" + username + "</b>");
 		$(".username-container span span:nth-child(2)").html("[" + tokenCount + " tokens]");
 		$("nav ul li:nth-child(1)").html("<a href='javascript:logOff();'>Log off</a>");
+		$("nav ul li:nth-child(2):not(.selected)").attr("href", "/index.html");
+		$("nav ul li:nth-child(3):not(.selected)").attr("href", "/results.html");
+		$("nav ul li:nth-child(4):not(.selected)").attr("href", "/profile.html");
+		$("nav ul li:nth-child(5):not(.selected)").attr("href", "/cards.html");
+		$("nav ul li:nth-child(6):not(.selected)").attr("href", "/games.html");
 	} else {
 		$(".username-container span span:nth-child(1)").text("You are logged off.");
 		$(".username-container span span:nth-child(2)").html("<a href='javascript:attemptLogIn(username, tokenCount);'>[Sign In]</a>");
+		$("nav ul li").each(function() {
+			$(this).attr("href", "");
+		});
 		$("nav ul li:nth-child(1)").html("<a href='javascript:attemptLogIn(username, tokenCount);'>Sign in</a>");
 		loggedIn = true;
 	}
