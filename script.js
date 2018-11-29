02b90f20a1189858ea6437a874f6eef9a33bcc8d
matetipping <matetipping@gmail.com>
2018-11-29 11:17:42 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 	$('#nav-hamburger').click(function(){
-		$(this).toggleClass('open');
+		if ($(this).hasClass("animcomplete")) {
+			$(this).removeClass("animcomplete");
+			$(this).addClass("closed");
+			setTimeout(function() {
+				$("#top_hamburger").removeClass("closed");
+			}, 700);
+		} else {
+		$(this).addClass("open");
+			setTimeout(function() {
+				$("#top_hamburger").removeClass("open");
+				$("#top_hamburger").addClass("animcomplete");
+			}, 700);
+		}
 	});
 });
