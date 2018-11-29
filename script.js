ec98b87ca0c6c76adc15b6399d4e1306a5204f22
matetipping <matetipping@gmail.com>
2018-11-29 14:38:56 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+	// testing only
+	username = "Your Username Here";
+	tokenCount = 478;
+	loggedIn = false;
+	attemptLogIn(username, tokenCount);
+	loggedIn = true;
+	// end test code
+	
+	// Hamburger menu
 	$("nav").css("right", (-1*$("nav").width()) + "px");
 	$("#nav-hamburger").click(function(){
 		if ($(this).hasClass("animcomplete")) {
@@ -17,4 +26,16 @@ $(document).ready(function(){
 			}, 500);
 		}
 	});
+	// End hamburger menu
+	
 });
+
+function attemptLogIn(username, tokenCount) {
+	if (loggedIn) {
+		$(".username-container span:nth-child(1)").text(username);
+		$(".username-container span:nth-child(2)").html("[" + tokenCount + " tokens]");
+	} else {
+		$(".username-container span:nth-child(1)").text("You are logged off.");
+		$(".username-container span:nth-child(2)").html("<a href="#">[Sign In]</a>");
+	}
+}
