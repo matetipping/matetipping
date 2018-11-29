f680e80f39af34b9e10a3e6895da666fb5014329
matetipping <matetipping@gmail.com>
2018-11-29 13:04:49 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ $(document).ready(function(){
 		if ($(this).hasClass("animcomplete")) {
 			$(this).removeClass("animcomplete");
 			$(this).addClass("closed");
+			$("nav").css("font-size", "0");
+			$("nav").css("top", "0");
+			$("nav").css("right", "-64px");
 			setTimeout(function() {
 				$("#nav-hamburger").removeClass("closed");
 			}, 500);
 		} else {
 		$(this).addClass("open");
+			$("nav").css("font-size", "16px");
+			$("nav").css("top", "64px");
+			$("nav").css("right", "0");
 			setTimeout(function() {
 				$("#nav-hamburger").removeClass("open");
 				$("#nav-hamburger").addClass("animcomplete");
