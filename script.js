d4f3f98a551606831bf7e2348d707860f18e5c38
matetipping <matetipping@gmail.com>
2019-04-11 18:01:06 +1000

    Update script.js

--- script.js
+++ script.js
@@ -188,9 +188,9 @@ function displayTippingForm() {
 				var venue = doc.data().venue;
 				var date = doc.data().date.toDate();
 				var formattedDate = getFormattedDate(date);
-				htmlFields = htmlFields + "<div class='details'><span class='align-left'>" + homeTeamLong + " vs " + awayTeamLong + "</span><span class='align-right'>" + venue + " | " + formattedDate + "</span></div>";
-				htmlFields = htmlFields + "<div class='flags'><div class='flag flag-" + i + "' id='NA'></div><select class='formInput' id='clubInput-" + i + "'><option disabled selected value></option><option id='home-" + i + "' value='" + homeTeam + "'>" + homeTeamLong + "</option><option id='away-" + i + "' value='" + awayTeam + "'>" + awayTeamLong + "</option><option id='draw-" + i + "' value='DRW'>Draw</option></select><input type='number' min='0' max='200' class='formInput' id='marginInput-" + i + "' value='0'></input></div>";
-				htmlFields = htmlFields + "<div class='slider'><input type='range' min='-5848' max='5848' class='formInput' id='marginSlider-" + i + "'></input></div>";
+				htmlFields = htmlFields + "<div class='game'><div class='details'><span class='align-left'>" + homeTeamLong + " vs " + awayTeamLong + "</span><span class='align-right'>" + venue + " | " + formattedDate + "</span></div>";
+				htmlFields = htmlFields + "<div class='flags'><div class='flag flag-" + i + "' id='NA'></div><select class='formInput' id='clubInput-" + i + "'><option disabled selected value style='display: none;'></option><option id='home-" + i + "' value='" + homeTeam + "'>" + homeTeamLong + "</option><option id='away-" + i + "' value='" + awayTeam + "'>" + awayTeamLong + "</option><option id='draw-" + i + "' value='DRW'>Draw</option></select><input type='number' min='0' max='200' class='formInput' id='marginInput-" + i + "' value='0'></input></div>";
+				htmlFields = htmlFields + "<div class='slider'><input type='range' min='-5848' max='5848' class='formInput' id='marginSlider-" + i + "'></input></div></div>";
 				i++;
 			});
 			$("#form-tipping").html(htmlFields);
