0c26d02082c5756a8df983abd9c5f07d6a63a490
matetipping <matetipping@gmail.com>
2020-02-06 22:31:43 +1100

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		}
 		var roundYear = new Date().getFullYear() + "-R" + text[0];
 		var roundCode = "R" + text[0];
-		var date = Date.parse(text[1]);
+		var date = firebase.firestore.Timestamp.fromDate(Date.parse(text[1]));
 		firebase.firestore().collection("rounds").doc(roundYear).set({
 			date: date,
 			codename: roundCode,
@@ -23,7 +23,7 @@ $(document).ready(function(){
 		var length = text.length;
 		for (i = 2; i < length; i++) {
 			var line = text[i].split(", ");
-			firebase.firestore().collection("rounds").doc(roundYear).collection("fixtures").doc(i - 1).set({
+			firebase.firestore().collection("rounds").doc(roundYear).collection("fixtures").doc((i - 1).toString()).set({
 				homeTeam: line[0],
 				awayTeam: line[1],
 				date: line[2],
