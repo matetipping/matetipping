686500709dee5107543477450607a3ce5e5f6158
matetipping <matetipping@gmail.com>
2020-02-06 21:57:55 +1100

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
 	$("button").click(function() {
 		var text = ("textarea").val().split("\n");
-    var roundYear = new Date().getFullYear() + "-R" + text[0];
-    var roundCode = "R" + text[0];
-    var roundName = "Round " + text[0];
-    var date = Date.parse(text[1]);
-    firebase.firestore().collection("rounds").doc(roundYear).set({
+		var roundYear = new Date().getFullYear() + "-R" + text[0];
+		var roundCode = "R" + text[0];
+		var roundName = "Round " + text[0];
+		var date = Date.parse(text[1]);
+		firebase.firestore().collection("rounds").doc(roundYear).set({
 			date: date,
 			codename: roundCode,
 			name: roundName
@@ -16,16 +16,16 @@ $(document).ready(function(){
 		    console.error("Error writing document: ", error);
 		});
     
-    var i;
-    var length = text.length;
-    for (i = 2; i < length; i++) {
-      var line = text.split(", ");
-      firebase.firestore().collection("rounds").doc(roundYear).collection("fixtures").doc(i - 1).set({
-        homeTeam: line[0],
-        awayTeam: line[1],
-        date: line[2],
-        venue: line[3]
-      });
-    }
+	var i;
+	var length = text.length;
+	for (i = 2; i < length; i++) {
+		var line = text.split(", ");
+      		firebase.firestore().collection("rounds").doc(roundYear).collection("fixtures").doc(i - 1).set({
+        		homeTeam: line[0],
+        		awayTeam: line[1],
+        		date: line[2],
+        		venue: line[3]
+      		});
+    	}
 
 });
