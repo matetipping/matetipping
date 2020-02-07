6396ed79c41012c46a3b27f5c008def96171ad06
matetipping <matetipping@gmail.com>
2020-02-07 20:07:28 +1100

    Update script.js

--- script.js
+++ script.js
@@ -454,10 +454,12 @@ function displayTippingForm() {
 				console.log(bonusScorer);
 				if (bonusDisposal !== null) {
 					var bonusValue = $("datalist.players option#" + bonusDisposal).html();
+					console.log(bonusValue);
 					$("#bonusInput-1").val(bonusValue);
 				}
 				if (bonusScorer !== null) {
 					var bonusValue = $("datalist.players option#" + bonusScorer).html();
+					console.log(bonusValue);
 					$("#bonusInput-2").val(bonusValue);
 				}
 				$("button.submit").html("Update Tips");
