3b435908a433447a106fe1273c438175f10086b3
matetipping <matetipping@gmail.com>
2020-02-07 13:33:10 +1100

    Update script.js

--- script.js
+++ script.js
@@ -302,7 +302,7 @@ function displayTippingForm() {
 					var length = playerList.length;
 					var dataListHTML = "";
 					for (i = 0; i < length; i++) {
-						dataListHTML = dataListHTML + "<option value='" + i + "' id='" + i + "'>" + playerList[i].name + " (" + playerList[i].club + ")</option>"
+						dataListHTML = dataListHTML + "<option id='" + i + "'>" + playerList[i].name + " (" + playerList[i].club + ")</option>"
 					}
 					$("datalist#players").html(dataListHTML);
 				} else {
