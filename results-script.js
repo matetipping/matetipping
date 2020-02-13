a8cd99c934d6ab4f4990863db54d7979b94cefcd
matetipping <matetipping@gmail.com>
2020-02-13 11:55:20 +1100

    Update results-script.js

--- results-script.js
+++ results-script.js
@@ -1,24 +1,24 @@
-var user = firebase.auth().currentUser; 
+
 
 function loadPageData() {
-  var db = firebase.firestore();
-  var inALeague = false;
-  var myLeagues = [];
+  	var db = firebase.firestore();
+	var user = firebase.auth().currentUser; 
+  	var inALeague = false;
+  	var myLeagues = [];
 	db.collection("leagues").where("participants", "array-contains", user.uid).get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      var creator = doc.data().creator;
-      var name = doc.data().name;
-      myLeagues.push(doc.id);
-      if (creator == user.uid) {
-        name = name + " ★";
-      }
-      $("div#leaguesList").append("<div>" + name + "</div>");
-    });
-    if (myLeagues.length == 0) {
-      $("div#leaguesList").append("<div>You are not currently in any leagues.</div>");
-    }
-  }).catch(function(error) {
-    console.log("Error retrieving leagues");
-  });
-
+    		querySnapshot.forEach(function(doc) {
+      			var creator = doc.data().creator;
+			var name = doc.data().name;
+			myLeagues.push(doc.id);
+		  	if (creator == user.uid) {
+				name = name + " ★";
+			}
+			$("div#leaguesList").append("<div>" + name + "</div>");
+		});
+		if (myLeagues.length == 0) {
+			$("div#leaguesList").append("<div>You are not currently in any leagues.</div>");
+		}
+  	}).catch(function(error) {
+    		console.log("Error retrieving leagues");
+	});
 }
