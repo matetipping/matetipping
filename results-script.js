d97d72f3e5533e75bdad89c5a6e8dd34b90e1bc5
matetipping <matetipping@gmail.com>
2020-02-13 11:45:45 +1100

    Update results-script.js

--- results-script.js
+++ results-script.js
@@ -1,3 +1,22 @@
 function loadPageData() {
+  var db = firebase.firestore();
+  var inALeague = false;
+  var myLeagues = [];
+	db.collection("leagues").where("participants", "array-contains", user.uid).get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      var creator = doc.data().creator;
+      var name = doc.data().name;
+      myLeagues.push(doc.id);
+      if (creator == user.uid) {
+        name = name + " ★";
+      }
+      $("div#leaguesList").append("<div>" + name + "</div>");
+    });
+    if (myLeagues.length == 0) {
+      $("div#leaguesList").append("<div>You are not currently in any leagues.</div>");
+    }
+  }).catch(function(error) {
+    console.log("Error retrieving leagues");
+  });
 
 }
