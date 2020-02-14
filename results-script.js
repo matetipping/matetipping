0ea96b76a67d6dbed28a04b5d5ca7304488b4ba6
matetipping <matetipping@gmail.com>
2020-02-14 15:53:49 +1100

    Update results-script.js

--- results-script.js
+++ results-script.js
@@ -98,7 +98,7 @@ function updateResults(doc) {
 		var opponentIndex = Number(fixtures[playerIndex].split(", ")[roundIndex]);
 		var myTipsRef = db.collection("users").doc(user.uid).collection("tips").doc(roundCode);
 		var opponentTipsRef = db.collection("users").doc(participants[opponentIndex]).collection("tips").doc(roundCode);
-		var results = db.collection("rounds").doc(roundCode);
+		var resultsRef = db.collection("rounds").doc(roundCode);
 		var myTipData = null;
 		var opponentTipData = null;
 		var resultsData = null;
@@ -108,7 +108,7 @@ function updateResults(doc) {
 		opponentTipsRef.get().then(function(doc) {
 			opponentTipData = doc.data();
 		});
-		results.get().then(function(doc) {
+		resultsRef.get().then(function(doc) {
 			resultsData = doc.data();
 			calculateScores(myTipData, opponentTipData, resultsData);
 		});
@@ -124,7 +124,7 @@ function updateLadder(doc) {
 	}
 }
 
-function resultsData(doc) {
+function calculateScores(myTips, oppTips, results) {
 	console.log("Calculate results here");
 }
 
