f9a0ad19f46f0d0f0ddf04029da71e80e05aa0d0
matetipping <matetipping@gmail.com>
2020-02-19 21:58:54 +1100

    Update profile-script.js

--- profile-script.js
+++ profile-script.js
@@ -8,26 +8,23 @@ $(document).ready(function() {
                 }
                 if ($(this).hasClass("club")) {
                         var clubs = ["ade", "bri", "car", "col", "ess", "fre", "gee", "gcs", "gws", "haw", "mel", "nth", "pta", "ric", "stk", "syd", "wce", "wbd"];
-                        var currentClub = $("avatar-display img#club").attr("src").split("-")[1].split(".")[0];
+                        var currentClub = $("div.avatar-display img#club").attr("src").split("-")[1].split(".")[0];
                         console.log(currentClub);
                         var index = clubs.indexOf(currentClub) + iteration;
                         if (index >= clubs.length) {
                                 index = 0;
-                        } else if (index == 0) {
+                        } else if (index < 0) {
                                 index = clubs.length - 1;
                         }
                         $("div.avatar-display img#club").attr("src", "/images/profile/jumper-" + clubs[index] + ".svg");
-                } else if ($(this).hasClass("club")) {
-                        var clubs = ["ade", "bri", "car", "col", "ess", "fre", "gee", "gcs", "gws", "haw", "mel", "nth", "pta", "ric", "stk", "syd", "wce", "wbd"];
-                        var currentClub = $("avatar-display img#club").attr("src").split("-")[1].split(".")[0];
-                        console.log(currentClub);
-                        var index = clubs.indexOf(currentClub) + iteration;
-                        if (index >= clubs.length) {
-                                index = 0;
+                } else if ($(this).hasClass("body")) {
+                        var index = $("div.avatar-display img#body").attr("src").split("-")[1].split(".")[0] + iteration;
+                        if (index > 5) {
+                                index = 1;
                         } else if (index == 0) {
-                                index = clubs.length - 1;
+                                index = 5;
                         }
-                        $("div.avatar-display img#club").attr("src", "/images/profile/jumper-" + clubs[index] + ".svg");
+                        $("div.avatar-display img#body").attr("src", "/images/profile/body-" + index + ".svg");
                 }
         });
 });
