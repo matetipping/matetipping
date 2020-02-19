20d0769ca11334c2e69ac29a8c18bfd9567007e6
matetipping <matetipping@gmail.com>
2020-02-19 21:55:54 +1100

    Update profile-script.js

--- profile-script.js
+++ profile-script.js
@@ -1,20 +1,33 @@
-$("div.avatar-controls button").click(function() {
-        console.log("hi");
-        var iteration = 1;
-        if ($(this).hasClass("back")) {
-                iteration = -1;
-                console.log("back");
-        }
-        if ($(this).hasClass("club")) {
-                var clubs = ["ade", "bri", "car", "col", "ess", "fre", "gee", "gcs", "gws", "haw", "mel", "nth", "pta", "ric", "stk", "syd", "wce", "wbd"];
-                var currentClub = $("avatar-display img#club").attr("src").split("-")[1].split(".")[0];
-                console.log(currentClub);
-                var index = clubs.indexOf(currentClub) + iteration;
-                if (index >= clubs.length) {
-                        index = 0;
-                } else if (index == 0) {
-                        index = clubs.length - 1;
+$(document).ready(function() {
+        $("div.avatar-controls button").click(function() {
+                console.log("hi");
+                var iteration = 1;
+                if ($(this).hasClass("back")) {
+                        iteration = -1;
+                        console.log("back");
                 }
-                $("div.avatar-display img#club").attr("src", "/images/profile/jumper-" + clubs[index] + ".svg");
-        }
+                if ($(this).hasClass("club")) {
+                        var clubs = ["ade", "bri", "car", "col", "ess", "fre", "gee", "gcs", "gws", "haw", "mel", "nth", "pta", "ric", "stk", "syd", "wce", "wbd"];
+                        var currentClub = $("avatar-display img#club").attr("src").split("-")[1].split(".")[0];
+                        console.log(currentClub);
+                        var index = clubs.indexOf(currentClub) + iteration;
+                        if (index >= clubs.length) {
+                                index = 0;
+                        } else if (index == 0) {
+                                index = clubs.length - 1;
+                        }
+                        $("div.avatar-display img#club").attr("src", "/images/profile/jumper-" + clubs[index] + ".svg");
+                } else if ($(this).hasClass("club")) {
+                        var clubs = ["ade", "bri", "car", "col", "ess", "fre", "gee", "gcs", "gws", "haw", "mel", "nth", "pta", "ric", "stk", "syd", "wce", "wbd"];
+                        var currentClub = $("avatar-display img#club").attr("src").split("-")[1].split(".")[0];
+                        console.log(currentClub);
+                        var index = clubs.indexOf(currentClub) + iteration;
+                        if (index >= clubs.length) {
+                                index = 0;
+                        } else if (index == 0) {
+                                index = clubs.length - 1;
+                        }
+                        $("div.avatar-display img#club").attr("src", "/images/profile/jumper-" + clubs[index] + ".svg");
+                }
+        });
 });
