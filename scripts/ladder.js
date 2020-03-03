ddf77991a50d22be59b50fd0e606f16f20d86bbf
matetipping <matetipping@gmail.com>
2020-03-03 23:54:54 +1100

    Update ladder.js

--- scripts/ladder.js
+++ scripts/ladder.js
@@ -1,29 +1,6 @@
 $(document).ready(function(){
         $("div#profileSave button.submit").click(function() {
-                var ladder = [];
-                var htmlBefore = $("button.submit").parent().html();
-                $("button.submit").replaceWith("<div class='loader'></div>");
-                $("div#ladderPrediction div.flag").each(function() {
-                        var thisID = $(this).attr("id");
-                        if (typeof thisID !== typeof undefined && thisID !== false) {
-                                ladder.push(thisID);
-                        }
-                });
-                if (ladder.length == 18) {
-                        firebase.firestore().collection("users").doc(user.uid).collection("preferences").doc("profile").update({
-                                ladderPrediction: ladder
-                        }).then(function() {
-                                displaySuccess("Ladder saved successfully.");
-                                $("div.loader").replaceWith(htmlBefore); 
-                        }).catch(function(e) {
-                                displayError("Could not save ladder.");
-                                $("div.loader").replaceWith(htmlBefore);
-                        });
-                        console.log(ladder);
-                } else {
-                        displayError("Must include all clubs.");
-                        $("div.loader").replaceWith(htmlBefore);
-                }
+                submitLadder();
         });
         $("div#ladderPrediction td.shiftUp").click(function() {
                 var thisPosition = Number($(this).parent().attr("id"));
@@ -43,6 +20,43 @@ $(document).ready(function(){
                 $("td.name-" + thisPosition).html(getLongName(thisID));
         });
 });
+
+function submitLadder() {
+        var ladder = [];
+        var htmlBefore = $("button.submit").parent().html();
+        $("button.submit").replaceWith("<div class='loader'></div>");
+        $("div#ladderPrediction div.flag").each(function() {
+                var thisID = $(this).attr("id");
+                if (typeof thisID !== typeof undefined && thisID !== false) {
+                        ladder.push(thisID);
+                }
+        });
+        if (ladder.length == 18) {
+                firebase.firestore().collection("users").doc(user.uid).collection("preferences").doc("profile").update({
+                        ladderPrediction: ladder
+                }).then(function() {
+                        displaySuccess("Ladder saved successfully.");
+                        $("div.loader").replaceWith(htmlBefore);
+                        $("div#profileSave button.submit").click(function() {
+                                submitLadder();
+                        });
+                        
+                }).catch(function(e) {
+                        displayError("Could not save ladder.");
+                        $("div.loader").replaceWith(htmlBefore);
+                        $("div#profileSave button.submit").click(function() {
+                                submitLadder();
+                        });
+                });
+                console.log(ladder);
+        } else {
+                displayError("Must include all clubs.");
+                $("div.loader").replaceWith(htmlBefore);
+                $("div#profileSave button.submit").click(function() {
+                        submitLadder();
+                });
+        }
+}
 
 function swapOrder(a, b) {
   var idA = $("div.flag-" + a).attr("id");
