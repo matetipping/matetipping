acae6c42dee6ee0053a3d81ac6a2898754b3e121
matetipping <matetipping@gmail.com>
2020-03-03 11:47:01 +1100

    Create database-config.js

---
+++ scripts/database-config.js
@@ -0,0 +1,9 @@
+var config = {
+        apiKey: "AIzaSyBEduYbG9nwxiif677ot7WLAAG8AuRiqoI",
+        authDomain: "mate-tipping.firebaseapp.com",
+        databaseURL: "https://mate-tipping.firebaseio.com",
+        projectId: "mate-tipping",
+        storageBucket: "mate-tipping.appspot.com",
+        messagingSenderId: "650624140459"
+};
+firebase.initializeApp(config);
