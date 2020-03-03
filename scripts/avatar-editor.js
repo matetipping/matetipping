06eb775b32d457cff965184687c0659e320676ae
matetipping <matetipping@gmail.com>
2020-03-03 22:10:55 +1100

    Update and rename profile.js to avatar-editor.js

--- scripts/avatar-editor.js
+++ scripts/avatar-editor.js
@@ -43,19 +43,6 @@ $(document).ready(function() {
                 console.log(profileAvatar);
         });
         
-        $(".menu-major button").click(function() {
-                $(".menu-major button.selected").removeClass("selected");
-                $(this).addClass("selected");
-                var navValue = $(this).html();
-                if (navValue == "Ladder") {
-                        $("div#profile-content").load("modules/ladder.html");
-                        $.getScript("scripts/ladder.js");
-                } else if (navValue == "Avatar") {
-                        $("div#profile-content").load("modules/avatar-editor.html");
-                        $.getScript("scripts/avatar-editor.js");
-                }
-        });
-        
         $("div.avatar-controls button").click(function() {
                 console.log("hi");
                 var iteration = 1;
