d507d9351c9676cd836cb9f96206e41e8c635c1d
matetipping <matetipping@gmail.com>
2020-03-04 00:40:35 +1100

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,6 @@
  $(document).ready(function() {
-        $("div#profile-content").load("modules/avatar.html");
+        $("div#profile-content").html("<div class='avatar-display'></div>");
+        $("div.avatar-display").load("modules/avatar.html");
         $(".menu-major button").click(function() {
                 $(".menu-major button.selected").removeClass("selected");
                 $(this).addClass("selected");
@@ -11,7 +12,8 @@
                         $("div#profile-content").load("modules/avatar-editor.html");
                         $.getScript("scripts/avatar-editor.js");
                 } else if (navValue == "Profile") {
-                        $("div#profile-content").load("modules/avatar.html");
+                        $("div#profile-content").html("<div class='avatar-display'></div>");
+                        $("div.avatar-display").load("modules/avatar.html");
                 } else if (navValue == "Settings") {
                         $("div#profile-content").load("");
                 }
