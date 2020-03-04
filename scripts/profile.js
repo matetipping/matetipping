b00afded3b03605e45d0ddb457b035de7a842189
matetipping <matetipping@gmail.com>
2020-03-04 14:26:35 +1100

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -7,11 +7,13 @@
                 $(this).addClass("selected");
                 var navValue = $(this).html();
                 if (navValue == "Ladder") {
-                        $("div#profile-content").load("modules/ladder.html");
-                        $.getScript("scripts/ladder.js");
+                        $("div#profile-content").load("modules/ladder.html", function() {
+                                $.getScript("scripts/ladder.js");
+                        });
                 } else if (navValue == "Avatar") {
-                        $("div#profile-content").load("modules/avatar-editor.html");
-                        $.getScript("scripts/avatar-editor.js");
+                        $("div#profile-content").load("modules/avatar-editor.html", function() {
+                                $.getScript("scripts/avatar-editor.js");
+                        });
                 } else if (navValue == "Profile") {
                         $("div#profile-content").html("<div class='avatar-display'></div>");
                         $("div.avatar-display").load("modules/avatar.html");
