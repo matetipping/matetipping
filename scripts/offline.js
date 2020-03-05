f942a79b742d7beec65370a71908a79ca0b95fdb
matetipping <matetipping@gmail.com>
2020-03-05 21:17:18 +1100

    Update offline.js

--- scripts/offline.js
+++ scripts/offline.js
@@ -40,7 +40,8 @@ $(document).ready(function() {
 					$("#form-register div.loader.reg-load").replaceWith("<input type='submit' value='Register'>");
 				} else {
 					$("main").load("modules/avatar-editor.html", function() {
-					$.getScript("scripts/avatar-editor.js");
+						$.getScript("scripts/avatar-editor.js");
+					});
 				}
 				registerUser(formData);
 			} else {
