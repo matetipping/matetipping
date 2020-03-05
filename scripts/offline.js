a1c5f7d394bc39c20e6ab78a1888caf94d461e0f
matetipping <matetipping@gmail.com>
2020-03-05 20:47:58 +1100

    Update offline.js

--- scripts/offline.js
+++ scripts/offline.js
@@ -1,85 +1,56 @@
 $(document).ready(function() {
-    // Registration form
-    $("#form-register").submit(function(e) {
-      e.preventDefault();
-      $("#form-register input[type='submit']").replaceWith("<div class='loader reg-load'></div>");
+	// Registration form
+	$("#form-register").submit(function(e) {
+		e.preventDefault();
+		$("#form-register input[type='submit']").replaceWith("<div class='loader reg-load'></div>");
 
-      var isRegistrationError = false;
-      var registrationErrorMessage;
-      var formData = {
-        "username": $("#input-register-username").val(),
-        "email": $("#input-register-email").val(),
-        "password": $("#input-register-password").val(),
-        "passwordConfirm": $("#input-register-password-confirm").val()
-      }
-      if (formData.username.length < 3 || formData.username.length > 20) {
-        isRegistrationError = true;
-        registrationErrorMessage = "Username must be between 3 and 20 characters.";
-	fixFields($("#input-register-username"));
-      } else if (!formData.email.includes("@")) {
-        isRegistrationError = true;
-        registrationErrorMessage = "Email address is invalid.";
-	fixFields($("#input-register-email"));
-      } else if (formData.password.length < 6) {
-        isRegistrationError = true;
-        registrationErrorMessage = "Password is too short.";
-	fixFields($("#input-register-password"));
-	fixFields($("#input-register-password-confirm"));
-      } else if (formData.password != formData.passwordConfirm) {
-        isRegistrationError = true;
-        registrationErrorMessage = "Passwords do not match.";
-	fixFields($("#input-register-password-confirm"));
-      }
-      if (isRegistrationError) {
-        displayError(registrationErrorMessage);
-        $("#form-register div.loader.reg-load").replaceWith("<input type='submit' value='Register'>");
-      } else {
-        commitLogOff();
-        firebase.auth().createUserWithEmailAndPassword(formData.email, formData.password).catch(function(error) {
-          var isRegistrationError = true;
-          if (error.code == "auth/email-already-in-use") {
-            displayError("Email address already in use.");
-	    fixFields($("#input-register-password"));
-	    fixFields($("#input-register-password-confirm"));
-          } else {
-            displayError("Registration failed unexpectedly.");
-          }
-          $("#form-register div.loader.reg-load").replaceWith("<input type='submit' value='Register'>");
-        });
-        if (!isRegistrationError) {
-          firebase.auth().onAuthStateChanged(function(user) {
-            username = formData.username;
-            if (user) {
-              user.updateProfile({
-                displayName: username
-              }).then(function() {
-		displaySuccess("Registration successful.");
-                localStorage.setItem('username', username);
-                //displayLogIn(user.displayName);
-              }, function(error) {
-                displayError("Failed to save username.");
-                $("#form-register div.loader.reg-load").replaceWith("<input type='submit' value='Register'>");		
-              });
-              var userRef = firebase.firestore().collection("users").doc(user.uid);
-              var profileRef = userRef.collection("preferences").doc("profile");
-              var batch = firebase.firestore().batch();
-
-              batch.set(userRef, {
-                admin: false,
-                ownedLeague: null
-              });
-              batch.set(profileRef, {
-                displayName: formData.username
-              });
-              batch.commit().then(function() {
-                console.log("User data set!");
-              }).catch(function(e) {
-                displayError("Failed to save important data.");
-              });
-            }
-          });
-        }
-      }
+		var isRegistrationError = false;
+		var registrationErrorMessage;
+		var formData = {
+			"username": $("#input-register-username").val(),
+			"email": $("#input-register-email").val(),
+			"password": $("#input-register-password").val(),
+			"passwordConfirm": $("#input-register-password-confirm").val()
+		}
+		if (formData.username.length < 3 || formData.username.length > 20) {
+			isRegistrationError = true;
+			registrationErrorMessage = "Username must be between 3 and 20 characters.";
+			fixFields($("#input-register-username"));
+		} else if (!formData.email.includes("@")) {
+			isRegistrationError = true;
+			registrationErrorMessage = "Email address is invalid.";
+			fixFields($("#input-register-email"));			
+		} else if (formData.password.length < 6) {			
+			isRegistrationError = true;
+			registrationErrorMessage = "Password is too short.";
+			fixFields($("#input-register-password"));
+			fixFields($("#input-register-password-confirm"));
+		} else if (formData.password != formData.passwordConfirm) {
+			isRegistrationError = true;
+			registrationErrorMessage = "Passwords do not match.";
+			fixFields($("#input-register-password-confirm"));
+		}
+		firebase.auth().fetchSignInMethodsForEmail(formData.email).catch(function(error) {
+			isRegistrationError = true;
+			registrationErrorMessage = "Email address already in use.";
+			fixFields($("#input-register-password"));
+			fixFields($("#input-register-password-confirm"));
+		});
+		if (isRegistrationError) {
+			displayError(registrationErrorMessage);
+			$("#form-register div.loader.reg-load").replaceWith("<input type='submit' value='Register'>");
+		} else {
+			username = formData.username;
+			var userProfile = {};
+			userProfile.displayName = formData.username;
+			var dbUserInfo = {};
+			dbUserInfo.admin = false;
+			dbUserInfo.ownedLeague = null;
+			var dbUserPrefs = {};
+			dbUserPrefs.displayName = formData.username;
+			commitLogOff();
+			registerUser(formData.email, formData.password, username, userProfile, dbUserInfo, dbUserPrefs);
+		}
     });
     
     $("span#resendEmail").parent().on("click", "span#resendEmail", function() {
@@ -131,3 +102,31 @@ $(document).ready(function() {
       });
     });
   });
+
+function registerUser(email, password, username, userProfile, dbUserInfo, dbUserPrefs) {
+	commitLogOff();
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		displayError("Registration failed unexpectedly.");
+	});
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			user.updateProfile(userProfile).then(function() {
+				displaySuccess("Registration successful.");
+				localStorage.setItem('username', username);
+			}, function(error) {
+				displayError("Failed to save username.");
+				$("#form-register div.loader.reg-load").replaceWith("<input type='submit' value='Register'>");		
+			});
+			var userRef = firebase.firestore().collection("users").doc(user.uid);
+			var profileRef = userRef.collection("preferences").doc("profile");
+			var batch = firebase.firestore().batch();
+			batch.set(userRef, dbUserInfo);
+			batch.set(profileRef, dbUserPrefs);
+			batch.commit().then(function() {
+				console.log("User data set!");
+			}).catch(function(e) {
+				displayError("Failed to save important data.");
+			});
+		}
+	});
+}
