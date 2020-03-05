c9a4bc7e34d3df58639b00448a3ed0fc99829cf4
matetipping <matetipping@gmail.com>
2020-03-05 18:16:28 +1100

    Update ladder.js

--- scripts/ladder.js
+++ scripts/ladder.js
@@ -3,9 +3,33 @@ $(document).ready(function(){
                 "This ladder will be used to generate default tips in matches where you fail to tip " +
                 "(you cannot win such matches but your percentage will be affected). " +
                 "It is also used as a finals tie-breaker.");
-        $("div#profileSave button.submit").click(function() {
-                submitLadder();
+        
+        $("div#profileSave").on("click", "button.submit", function() {
+                var ladder = [];
+                var htmlBefore = startLoad($("button.submit"));
+                $("div#ladderPrediction div.flag").each(function() {
+                        var thisID = $(this).attr("id");
+                        if (typeof thisID !== typeof undefined && thisID !== false) {
+                                ladder.push(thisID);
+                        }
+                });
+                if (ladder.length == 18) {
+                        firebase.firestore().collection("users").doc(user.uid).collection("preferences").doc("profile").update({
+                                ladderPrediction: ladder
+                        }).then(function() {
+                                displaySuccess("Ladder saved successfully.");
+                                endLoad(htmlBefore);
+                        }).catch(function(e) {
+                                displayError("Could not save ladder.");
+                                endLoad(htmlBefore);
+                        });
+                        console.log(ladder);
+                } else {
+                        displayError("Must include all clubs.");
+                        endLoad(htmlBefore);
+                }
         });
+        
         $("div#ladderPrediction td.shiftUp").click(function() {
                 var thisPosition = Number($(this).parent().attr("id"));
                 var otherPosition = thisPosition - 1;
@@ -24,43 +48,6 @@ $(document).ready(function(){
                 $("td.name-" + thisPosition).html(getLongName(thisID));
         });
 });
-
-function submitLadder() {
-        var ladder = [];
-        var htmlBefore = $("button.submit").parent().html();
-        $("button.submit").replaceWith("<div class='loader'></div>");
-        $("div#ladderPrediction div.flag").each(function() {
-                var thisID = $(this).attr("id");
-                if (typeof thisID !== typeof undefined && thisID !== false) {
-                        ladder.push(thisID);
-                }
-        });
-        if (ladder.length == 18) {
-                firebase.firestore().collection("users").doc(user.uid).collection("preferences").doc("profile").update({
-                        ladderPrediction: ladder
-                }).then(function() {
-                        displaySuccess("Ladder saved successfully.");
-                        $("div.loader").replaceWith(htmlBefore);
-                        $("div#profileSave button.submit").click(function() {
-                                submitLadder();
-                        });
-                        
-                }).catch(function(e) {
-                        displayError("Could not save ladder.");
-                        $("div.loader").replaceWith(htmlBefore);
-                        $("div#profileSave button.submit").click(function() {
-                                submitLadder();
-                        });
-                });
-                console.log(ladder);
-        } else {
-                displayError("Must include all clubs.");
-                $("div.loader").replaceWith(htmlBefore);
-                $("div#profileSave button.submit").click(function() {
-                        submitLadder();
-                });
-        }
-}
 
 function swapOrder(a, b) {
   var idA = $("div.flag-" + a).attr("id");
