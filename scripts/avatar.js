e021f050270a7831ec3bafa962fc4b52cd4fa2e0
matetipping <matetipping@gmail.com>
2021-03-12 17:38:18 +1100

    Update avatar.js

--- scripts/avatar.js
+++ scripts/avatar.js
@@ -5,46 +5,46 @@ $(document).ready(function() {
   var myRef = db.collection("users").doc(firebase.auth().currentUser.uid).collection("preferences").doc("profile");
   myRef.get().then(function(doc) {
     playerAvatarData = doc.data().avatar;
+    if (firebase.auth().currentUser) {
+      var avatarHTML = localStorage.getItem("avatar");
+      if (avatarHTML !== null && avatarHTML !== "undefined") {
+        $(".avatar-display").html(avatarHTML);
+      }
+      firebase.firestore().collection("users").doc(user.uid).collection("preferences").doc("profile").get().then(function(doc) {
+          if (doc.exists) {
+              try {
+                setInitialGlobalVariables(doc.data().avatar);
+              } catch(err) {}
+              $("div#avatar-user img#hairback").attr("src", "/images/profile/hairback-" + doc.data().avatar.hairstyle + ".svg");
+              $("div#avatar-user img#body").attr("src", "/images/profile/body-" + doc.data().avatar.body + ".svg");
+              $("div#avatar-user img#club").attr("src", "/images/profile/jumper-" + doc.data().avatar.club + ".svg");
+              $("div#avatar-user img#head").attr("src", "/images/profile/head-" + doc.data().avatar.head + ".svg");
+              $("div#avatar-user img#freckles").attr("src", "/images/profile/freckles-" + doc.data().avatar.freckles + ".svg");
+              $("div#avatar-user img#wrinkles").attr("src", "/images/profile/wrinkles-" + doc.data().avatar.wrinkles + ".svg");
+              $("div#avatar-user img#facialhair").attr("src", "/images/profile/facialhair-" + doc.data().avatar.head + "-" + doc.data().avatar.facialHair + ".svg");
+              $("div#avatar-user img#mouth").attr("src", "/images/profile/mouth-" + doc.data().avatar.mouth + ".svg");
+              $("div#avatar-user img#eyebrows").attr("src", "/images/profile/eyebrows-" + doc.data().avatar.eyebrows + ".svg");
+              $("div#avatar-user img#eyelashes").attr("src", "/images/profile/eyelashes-" + doc.data().avatar.eyelashes + ".svg");
+              $("div#avatar-user img#nose").attr("src", "/images/profile/nose-" + doc.data().avatar.nose + ".svg");
+              $("div#avatar-user img#glasses").attr("src", "/images/profile/glasses-" + doc.data().avatar.glasses + ".svg");
+              $("div#avatar-user img#hairstyle").attr("src", "/images/profile/hair-" + doc.data().avatar.head + "-" + doc.data().avatar.hairstyle + ".svg");
+              $("div#avatar-user img#bandages").attr("src", "/images/profile/bandages-" + doc.data().avatar.bandages + ".svg");
+              $("div#avatar-user img.hair").css("filter", doc.data().avatar.hairColour);
+              $("div#avatar-user img.skin").css("filter", doc.data().avatar.skinColour);
+              $("div#avatar-user img.facialhair").css("filter", doc.data().avatar.facialHairColour);
+          } else {
+              setDefaultAvatar();
+          }
+          if ($(".avatar-display").html() !== undefined) {
+            localStorage.setItem("avatar", $(".avatar-display").html());
+          }
+        });
+     } else {
+       setDefaultAvatar();
+     }
+     setAvatar(playerAvatarData, "player");
+     setAvatar(opponentAvatarData, "opponent");
   });
-  if (firebase.auth().currentUser) {
-    var avatarHTML = localStorage.getItem("avatar");
-    if (avatarHTML !== null && avatarHTML !== "undefined") {
-      $(".avatar-display").html(avatarHTML);
-    }
-    firebase.firestore().collection("users").doc(user.uid).collection("preferences").doc("profile").get().then(function(doc) {
-        if (doc.exists) {
-            try {
-              setInitialGlobalVariables(doc.data().avatar);
-            } catch(err) {}
-            $("div#avatar-user img#hairback").attr("src", "/images/profile/hairback-" + doc.data().avatar.hairstyle + ".svg");
-            $("div#avatar-user img#body").attr("src", "/images/profile/body-" + doc.data().avatar.body + ".svg");
-            $("div#avatar-user img#club").attr("src", "/images/profile/jumper-" + doc.data().avatar.club + ".svg");
-            $("div#avatar-user img#head").attr("src", "/images/profile/head-" + doc.data().avatar.head + ".svg");
-            $("div#avatar-user img#freckles").attr("src", "/images/profile/freckles-" + doc.data().avatar.freckles + ".svg");
-            $("div#avatar-user img#wrinkles").attr("src", "/images/profile/wrinkles-" + doc.data().avatar.wrinkles + ".svg");
-            $("div#avatar-user img#facialhair").attr("src", "/images/profile/facialhair-" + doc.data().avatar.head + "-" + doc.data().avatar.facialHair + ".svg");
-            $("div#avatar-user img#mouth").attr("src", "/images/profile/mouth-" + doc.data().avatar.mouth + ".svg");
-            $("div#avatar-user img#eyebrows").attr("src", "/images/profile/eyebrows-" + doc.data().avatar.eyebrows + ".svg");
-            $("div#avatar-user img#eyelashes").attr("src", "/images/profile/eyelashes-" + doc.data().avatar.eyelashes + ".svg");
-            $("div#avatar-user img#nose").attr("src", "/images/profile/nose-" + doc.data().avatar.nose + ".svg");
-            $("div#avatar-user img#glasses").attr("src", "/images/profile/glasses-" + doc.data().avatar.glasses + ".svg");
-            $("div#avatar-user img#hairstyle").attr("src", "/images/profile/hair-" + doc.data().avatar.head + "-" + doc.data().avatar.hairstyle + ".svg");
-            $("div#avatar-user img#bandages").attr("src", "/images/profile/bandages-" + doc.data().avatar.bandages + ".svg");
-            $("div#avatar-user img.hair").css("filter", doc.data().avatar.hairColour);
-            $("div#avatar-user img.skin").css("filter", doc.data().avatar.skinColour);
-            $("div#avatar-user img.facialhair").css("filter", doc.data().avatar.facialHairColour);
-        } else {
-            setDefaultAvatar();
-        }
-        if ($(".avatar-display").html() !== undefined) {
-          localStorage.setItem("avatar", $(".avatar-display").html());
-        }
-      });
-   } else {
-     setDefaultAvatar();
-   }
-   setAvatar(playerAvatarData, "player");
-   setAvatar(opponentAvatarData, "opponent");
 });
 
 function setAvatar(data, avatarType) {
